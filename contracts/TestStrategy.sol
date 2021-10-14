03b8fedc44cefde6d7964f84f56b46f7665e9e37
therealmonoloco <therealmonoloco@gmail.com>
2021-10-14 10:36:22 -0300

    test: add test strategy wrapper to expose internal methods

---
+++ contracts/TestStrategy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "./Strategy.sol";
+
+// The purpose of this wrapper contract is to expose internal functions
+// that may contain application logic and therefore need to be tested
+contract TestStrategy is Strategy {
+  
+      constructor(address _vault) public Strategy(_vault) {}
+
+      function sellLQTYforDAI() public {
+        _sellLQTYforDAI();
+      }
+
+      function sellETHforDAI() public {
+        _sellETHforDAI();
+      }
+
+      function sellDAIforLUSD() public {
+        _sellDAIforLUSD();
+      }
+}
