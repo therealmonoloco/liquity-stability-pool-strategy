89226ac1de91e555f82237eea7a373eb03b4deaf
therealmonoloco <therealmonoloco@gmail.com>
2021-10-18 14:51:27 -0400

    feat: use curve to convert dai to lusd

---
+++ interfaces/curve/IStableSwapExchange.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Copyright (c) Curve.Fi, 2021 - all rights reserved
+
+pragma solidity 0.6.12;
+
+interface IStableSwapExchange {
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external returns (uint256);
+}
